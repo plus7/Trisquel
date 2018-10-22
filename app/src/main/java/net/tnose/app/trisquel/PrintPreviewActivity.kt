713ccfe6732bfbe87444ee9c377d7302bbbb07f1
Takafumi NOSE <ahya365@gmail.com>
2018-10-22 21:07:51 +0900

    印刷プレビューをいろいろなおす。リファクタリングしたりスタイルを整備したり足りないフィールドを追加したり。

--- app/src/main/java/net/tnose/app/trisquel/PrintPreviewActivity.kt
+++ app/src/main/java/net/tnose/app/trisquel/PrintPreviewActivity.kt
@@ -36,31 +36,66 @@ class PrintPreviewActivity : AppCompatActivity() {
         val sb = StringBuilder()
         sb.append("<!doctype><html><head>")
         sb.append("<title>${name}</title>")
+        sb.append("<style>" +
+                "div.filminfo {\n" +
+                "  margin-bottom: 1em;\n" +
+                "}\n" +
+                "h2 {\n" +
+                "    margin:0px;\n" +
+                "    padding-top: 0.83em;\n" +
+                "    padding-bottom: 0.83em;\n" +
+                "}\n" +
+                "table {\n" +
+                "  border-collapse: collapse;\n" +
+                "}\n" +
+                "table th, table td {\n" +
+                "  border: solid 1px black;\n" +
+                "}\n" +
+                "div.photo {\n" +
+                "   page-break-inside: avoid;\n"+
+                "}\n"+
+                "div.content {\n" +
+                "   column-count: 2;\n"+
+                "}\n"+
+                "</style>")
+        //
+//         +
         sb.append("</head><body>")
 
-        sb.append("<h1>${name}</h1>")
-        sb.append("<table>")
+        sb.append("<div class=\"filminfo\"><h1>${name}</h1>")
+        sb.append("<table width=\"100%\">")
         sb.append("<tr><td>${getString(R.string.label_camera)}</td>      <td>${filmroll.camera.manufacturer} ${filmroll.camera.modelName}</td></tr>")
         sb.append("<tr><td>${getString(R.string.label_manufacturer)}</td> <td>${filmroll.manufacturer}</td></tr>")
         sb.append("<tr><td>${getString(R.string.label_brand)}</td>       <td>${filmroll.brand}</td></tr>")
         sb.append("<tr><td>${getString(R.string.label_iso)}</td>         <td>${filmroll.iso}</td></tr>")
-        sb.append("</table>")
+        sb.append("</table></div><div class=\"content\">")
 
         for (p in photos) {
-            sb.append("<h2>#${p.index}</h2>")
-            sb.append("<table>")
+            val accessories = p.accessories.map{a -> dao.getAccessory(a)!!.name }.joinToString(", ")
+            sb.append("<div class=\"photo\">")
+            sb.append("<h2>#${p.index+1}</h2>")
+            sb.append("<table width=\"100%\">")
             val lens = dao.getLens(p.lensid)
             sb.append("<tr><td>${getString(R.string.label_date)}</td>          <td>${p.date}</td></tr>")
             sb.append("<tr><td>${getString(R.string.label_lens)}</td>          <td>${lens!!.manufacturer} ${lens.modelName}</td></tr>")
-            sb.append("<tr><td>${getString(R.string.label_aperture)}</td>      <td>${p.aperture}</td></tr>")
-            sb.append("<tr><td>${getString(R.string.label_shutter_speed)}</td>  <td>${Util.doubleToStringShutterSpeed(p.shutterSpeed)}</td></tr>")
+            if(p.aperture > 0.0)
+                sb.append("<tr><td>${getString(R.string.label_aperture)}</td>      <td>${p.aperture}</td></tr>")
+            if(p.shutterSpeed > 0.0)
+                sb.append("<tr><td>${getString(R.string.label_shutter_speed)}</td>  <td>${Util.doubleToStringShutterSpeed(p.shutterSpeed)}</td></tr>")
             sb.append("<tr><td>${getString(R.string.label_ttl_light_meter)}</td><td>${p.ttlLightMeter}</td></tr>")
-            sb.append("<tr><td>${getString(R.string.label_location)}</td>      <td>${p.location}</td></tr>")
-            sb.append("<tr><td>${getString(R.string.label_memo)}</td>          <td>${p.memo}</td></tr>")
+            if(p.location.isNotEmpty())
+                sb.append("<tr><td>${getString(R.string.label_location)}</td>      <td>${p.location}</td></tr>")
+            if(p.isValidLatLng)
+                sb.append("<tr><td>${getString(R.string.label_coordinate)}</td>      <td>${p.latitude}, ${p.longitude}</td></tr>")
+            if(p.memo.isNotEmpty())
+                sb.append("<tr><td>${getString(R.string.label_memo)}</td>          <td>${p.memo}</td></tr>")
+            if(accessories.isNotEmpty())
+                sb.append("<tr><td>${getString(R.string.label_accessories)}</td>    <td>${accessories}</td></tr>")
             sb.append("</table>")
+            sb.append("</div>")
         }
 
-        sb.append("</body></html>")
+        sb.append("</div></body></html>")
         dao.close()
 
         webview.loadDataWithBaseURL(null, sb.toString(), "text/html", "UTF8", null)
