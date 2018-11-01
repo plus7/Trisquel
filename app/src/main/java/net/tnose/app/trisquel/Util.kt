2a89b13735844d1d777e5bf5294c1aa94105e20e
Takafumi NOSE <ahya365@gmail.com>
2018-11-01 23:01:43 +0900

    バージョンを5に上げる

--- app/src/main/java/net/tnose/app/trisquel/Util.kt
+++ app/src/main/java/net/tnose/app/trisquel/Util.kt
@@ -32,7 +32,7 @@ class Util {
     }
 
     companion object {
-        val TRISQUEL_VERSION = 4
+        val TRISQUEL_VERSION = 5
 
         /* シャッタースピードはたかだか2桁精度なのでdoubleからきれいに変換できる */
         internal fun doubleToStringShutterSpeed(ss: Double): String {
