de596fd726840f0137a1d03e8e67c412d7fe7f41
Takafumi NOSE <ahya365@gmail.com>
2021-05-03 06:13:35 +0900

    Release 18

--- app/src/main/java/net/tnose/app/trisquel/Util.kt
+++ app/src/main/java/net/tnose/app/trisquel/Util.kt
@@ -32,7 +32,7 @@ class Util {
     }
 
     companion object {
-        val TRISQUEL_VERSION = 17
+        val TRISQUEL_VERSION = 18
 
         /* シャッタースピードはたかだか2桁精度なのでdoubleからきれいに変換できる */
         internal fun doubleToStringShutterSpeed(ss: Double): String {
