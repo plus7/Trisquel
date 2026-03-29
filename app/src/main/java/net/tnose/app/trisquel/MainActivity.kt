9e227aed070c5279fb9bca0e5ff17e019302a173
Takafumi Nose <ahya365@gmail.com>
2026-03-30 00:42:19 +0900

    ビルドできなくなっていたので修正

--- app/src/main/java/net/tnose/app/trisquel/MainActivity.kt
+++ app/src/main/java/net/tnose/app/trisquel/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
