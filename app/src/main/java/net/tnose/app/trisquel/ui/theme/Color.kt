918674146ebb34b21a0b034566dec30eab3f5a21
Takafumi Nose <ahya365@gmail.com>
2026-03-31 00:23:06 +0900

    Theme関連ファイルのコミット忘れに気づいた。

---
+++ app/src/main/java/net/tnose/app/trisquel/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package net.tnose.app.trisquel.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Green = Color(0xFF008D56)
+val GreenDark = Color(0xFF125115)
+val GreenAccent = Color(0xFF008d56)
