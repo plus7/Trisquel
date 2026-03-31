a58182f72f9410bf25f66dee7422f45d9dbce4ac
Takafumi Nose <ahya365@gmail.com>
2026-03-31 20:43:55 +0900

    FavoritePhotoFragmentをもう使っていないので削除

--- app/src/main/java/net/tnose/app/trisquel/FavoritePhotoScreen.kt
+++ app/src/main/java/net/tnose/app/trisquel/FavoritePhotoScreen.kt
@@ -1,10 +1,5 @@
 package net.tnose.app.trisquel
 
-import android.content.Context
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -20,15 +15,10 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -36,97 +26,6 @@ import androidx.compose.ui.unit.sp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.bumptech.glide.integration.compose.placeholder
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-
-/**
- * A fragment representing a list of Items.
- * Activities containing this fragment MUST implement the
- * [FavoritePhotoFragment.OnListFragmentInteractionListener] interface.
- */
-class FavoritePhotoFragment : androidx.fragment.app.Fragment() {
-    private var columnCount = 3
-
-    private var listener: OnListFragmentInteractionListener? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            columnCount = it.getInt(ARG_COLUMN_COUNT)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return ComposeView(requireContext()).apply {
-            setContent {
-                MaterialTheme {
-                    val context = LocalContext.current
-                    val groupedPhotos = remember { mutableStateOf<List<Pair<String, List<Photo>>>>(emptyList()) }
-
-                    LaunchedEffect(Unit) {
-                        withContext(Dispatchers.IO) {
-                            val dao = TrisquelDao(context)
-                            dao.connection()
-                            val list = dao.getAllFavedPhotos()
-
-                            val map = list.groupBy { it.filmrollid }
-                            val list2 = map.values.sortedByDescending { it[0].date }
-
-                            val result = list2.map { l ->
-                                val sortedList = l.sortedBy { it.frameIndex }
-                                val filmrollName = dao.getFilmRoll(l[0].filmrollid)?.name ?: ""
-                                Pair(filmrollName, sortedList)
-                            }
-                            dao.close()
-                            
-                            withContext(Dispatchers.Main) {
-                                groupedPhotos.value = result
-                            }
-                        }
-                    }
-
-                    FavoritePhotoScreen(
-                        groupedPhotos = groupedPhotos.value,
-                        columnCount = columnCount,
-                        onItemClick = { photo, list -> listener?.onListFragmentInteraction(photo, list) }
-                    )
-                }
-            }
-        }
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnListFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnListFragmentInteractionListener")
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    interface OnListFragmentInteractionListener {
-        fun onListFragmentInteraction(item: Photo?, list: List<Photo?>)
-    }
-
-    companion object {
-        const val ARG_COLUMN_COUNT = "column-count"
-        @JvmStatic
-        fun newInstance(columnCount: Int) =
-                FavoritePhotoFragment().apply {
-                    arguments = Bundle().apply {
-                        putInt(ARG_COLUMN_COUNT, columnCount)
-                    }
-                }
-    }
-}
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
